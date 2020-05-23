e2bb15d902e2cd64b551ddb5b44d46530d01eb9e
hanif05 <muamarhanif05@gmail.com>
2020-05-23 22:43:50 +0700

    add lang label

---
+++ resources/lang/en/label.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    // admin
+    'category_table' => 'Categories Table',
+    'name' => 'Name',
+    'no_records' => 'No Records Found',
+    'action' => 'Action',
+];
---
+++ resources/lang/id/label.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    // admin
+    'category_table' => 'Tabel Kategori',
+    'name' => 'Nama',
+    'no_records' => 'Data Tidak Ditemukan',
+    'action' => 'Aksi',
+];
