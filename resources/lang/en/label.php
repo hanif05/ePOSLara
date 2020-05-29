b3531309f68035ffb69135fe46a56ddb766d3736
hanif05 <muamarhanif05@gmail.com>
2020-05-29 17:48:23 +0700

    update lang

--- resources/lang/en/label.php
+++ resources/lang/en/label.php
@@ -6,6 +6,7 @@ return [
     'category_table' => 'Categories Table',
     'category_name' => 'Category Name',
     'no_records' => 'No Records Found',
+    'category_select' => 'Select This For Main Category',
     'action' => 'Action',
     'name' => 'Name',
 ];
--- resources/lang/id/label.php
+++ resources/lang/id/label.php
@@ -5,6 +5,7 @@ return [
     'category_table' => 'Tabel Kategori',
     'category_name' => 'Nama Kategori',
     'no_records' => 'Data Tidak Ditemukan',
+    'category_select' => 'Pilih Ini Untuk Kategori Utama',
     'action' => 'Aksi',
     'name' => 'Nama',
 ];
