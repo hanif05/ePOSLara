52f4265bed6cea159c58a137c01006107dabce53
hanif05 <muamarhanif05@gmail.com>
2020-05-29 17:47:07 +0700

    add method parent child and mutators in model category

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $guarded = [];
+    
+    /**
+     * childs
+     *
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function childs()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+    
+    /**
+     * parent
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function getParentedNameAttribute()
+    {
+        $id = $this->parent_id;
+        if ($id > 0) {
+            $name = Category::findOrFail($id);
+            $name = $name->name;
+        } else {
+            $name = $this->name;
+        }
+
+        return $name;
+    }
 }
